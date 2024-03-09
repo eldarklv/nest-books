2b4506d581875edb4d41c2ceb336d889fc69347b
Ildar Kuluev <ildar.l.kuluev63@gmail.com>
2024-03-10 00:42:37 +0300

    fix lint

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ExtendBody } from './interceptors/extend.body.interceptor';
 import { HttpExceptionFilter } from './exceptionFilter/http-exception.filter';
+// import { ExtendBody } from './interceptors/extend.body.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
