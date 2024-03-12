3ceb2f9a2fcb680e92ae5379d881402c85569b2d
Ildar Kuluev <ildar.l.kuluev63@gmail.com>
2024-03-13 00:18:40 +0300

    do service unit tests

--- src/books/books.service.spec.ts
+++ src/books/books.service.spec.ts
@@ -3,7 +3,7 @@ import { BooksService } from './books.service';
 import { getModelToken } from '@nestjs/mongoose';
 import { Book } from './schemas/book.schema';
 import { BookDto } from './dto/book.dto';
-import { Model } from 'mongoose';
+import { Model, Types } from 'mongoose';
 
 describe('BooksService', () => {
   let bookService: BooksService;
@@ -15,7 +15,7 @@ describe('BooksService', () => {
     findById: jest.fn(),
     findByIdAndUpdate: jest.fn(),
     findByIdAndDelete: jest.fn(),
-    save: jest.fn(),
+    create: jest.fn(),
   };
 
   const mockBook = {
@@ -46,6 +46,22 @@ describe('BooksService', () => {
     expect(bookService).toBeDefined();
   });
 
+  it('create book', async () => {
+    // Почему так мок не сработал?
+    // jest.spyOn(modelBook, 'create').mockResolvedValue(mockBook);
+    const bookDto: BookDto = {
+      title: 'Master and Margarita',
+      description: 'Story about love',
+      authors: 'Bulgakov',
+      favorite: true,
+    };
+    const createdBook = { _id: '65ef79c8e816e5bfe895a41e', ...bookDto };
+    (modelBook.create as jest.Mock).mockResolvedValue(createdBook);
+
+    const result = await bookService.create(bookDto);
+    expect(result).toEqual(createdBook);
+  });
+
   it('find book', async () => {
     jest.spyOn(modelBook, 'findById').mockResolvedValue(mockBook);
 
@@ -53,5 +69,40 @@ describe('BooksService', () => {
 
     expect(createdBook).toBeDefined();
     expect(createdBook).toEqual(mockBook);
+  });
+
+  it('find all books', async () => {
+    jest.spyOn(modelBook, 'find').mockResolvedValue([mockBook, mockBook]);
+
+    const books = await bookService.findAll();
+
+    expect(books).toBeDefined();
+    expect(books).toHaveLength(2);
+    expect(books).toEqual([mockBook, mockBook]);
+  });
+
+  it('update book', async () => {
+    jest.spyOn(modelBook, 'findByIdAndUpdate').mockResolvedValue(mockBook);
+
+    const bookDto: BookDto = {
+      title: 'Master and Margarita',
+      description: 'Story about love',
+      authors: 'Bulgakov',
+      favorite: true,
+    };
+
+    const updatedBook = await bookService.update(mockBook._id, bookDto);
+
+    expect(updatedBook).toBeDefined();
+    expect(updatedBook).toEqual(mockBook);
+  });
+
+  it('delete book', async () => {
+    jest.spyOn(modelBook, 'findByIdAndDelete').mockResolvedValue(mockBook);
+
+    const deletedBook = await bookService.remove(mockBook._id);
+
+    expect(deletedBook).toBeDefined();
+    expect(deletedBook).toEqual(mockBook);
   });
 });
